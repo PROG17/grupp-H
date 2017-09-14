195c73ed00fb588ded333716ade4016c012b42c0
Alexander Panagiotis Arvanitis <31844503+Kamiljen@users.noreply.github.com>
2017-09-14 13:37:30 +0200

    Start

---
+++ Sudoku/Sudoku/Sudoku.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Sudoku
+{
+    class Sudoku
+    {
+        private string[] Board { get; set; }
+
+        public Sudoku(string boardString)
+        {
+            List<string> boardList = new List<string>();
+            for (int i = 0; i < boardList.lenght; i++)
+            {
+                
+            }
+        }
+
+    }
+
+}
