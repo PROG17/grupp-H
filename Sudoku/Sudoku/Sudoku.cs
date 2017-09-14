a6769c28c19f52f0e67ae35fdc16810f82cca406
Andreas <antenilsson87@hotmail.com>
2017-09-14 21:20:43 +0200

    Test

--- Sudoku/Sudoku/Sudoku.cs
+++ Sudoku/Sudoku/Sudoku.cs
@@ -6,6 +6,48 @@ using System.Threading.Tasks;
 
 namespace Sudoku.Sudoku
 {
+//    // Hjälpmetod till GetListOfNumbers som returnerar en sträng av alla nummer i den aktuella boxen
+//    private string GetBox(int x, int y)
+//    {
+//    List<int> yList = new List<int>();
+//        if (y < 3)
+//        yList.AddRange(new[] { 0, 1, 2 });
+//    else if (y < 6)
+//        yList.AddRange(new[] { 3, 4, 5 });
+//    else if (y < 9)
+//        yList.AddRange(new[] { 6, 7, 8 });
+
+//    if (x < 3)
+//    {
+//        return GetBoxValues(0, yList);
+//    }
+//    else if (x < 6)
+//    {
+//        return GetBoxValues(3, yList);
+//    }
+//    else
+//    {
+//        return GetBoxValues(6, yList);
+//    }
+//    }
+
+//    // Hjälpmetod till GetBox som hämtar en sträng baserad på aktuellt x och y värde
+//    private string GetBoxValues(int x, List<int> yList)
+//    {
+//    StringBuilder sb = new StringBuilder();
+
+//        for (int i = 0; i < 3; i++)
+//    {
+//        for (int j = 0; j < 3; j++)
+//        {
+//            sb.Append(Board[yList[i]][j + x]);
+//        }
+//    }
+
+//    return sb.ToString();
+//}
+
+
     class Sudoku
     {
         // En array som håller sudokut
